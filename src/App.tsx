699b3c4987ce0f646270a89421c438fe3f80c560
kewin <404502753@qq.com>
2020-08-19 22:13:12 +0800

    Button组件样式优化

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Button disabled>Hello</Button>
-        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>Hello</Button>
-        <Button btnType={ButtonType.Link} href="www.baidu.com" disabled>Hello</Button>
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>Primary  Lagre</Button>
+        <br />
+        <Button btnType={ButtonType.Danger} size={ButtonSize.Small}>Danger small </Button>
+        <br />
+        <Button btnType={ButtonType.Default} size={ButtonSize.Small}>Default Small </Button>
+        <br />
+        <Button disabled> Disabled Button</Button>
+        <br />
+        <Button btnType={ButtonType.Link} href="www.baidu.com" disabled>Disabled Link</Button>
+        <br />
+        <Button btnType={ButtonType.Link} href="www.baidu.com">Baidu Link</Button>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
