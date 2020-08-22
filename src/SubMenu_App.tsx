459fe043e40f1bf503b36d49b6d18f00b645ed29
kewin <404502753@qq.com>
2020-08-22 15:35:03 +0800

    利用forntawesome编写icon,使用scss中的@each语法生成icon颜色

--- src/SubMenu_App.tsx
+++ src/SubMenu_App.tsx
@@ -1,18 +1,12 @@
 import React from 'react';
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fas } from '@fortawesome/free-solid-svg-icons'
 import Menu from './componsnts/Menu/menu';
 import MenuItem from './componsnts/Menu/menuItem';
 import SubMenu from './componsnts/Menu/subMenu';
-import Icon from './componsnts/Icon/icon'
-
-library.add(fas)
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Icon icon="arrow-down" theme="primary" size="10x" />
         <Menu defaultIndex='0' mode="vertical" onSelect={(index) => { alert(index) }} defaultOpenSubMenus={['2']} >
           <MenuItem> cool link</MenuItem>
           <MenuItem disabled> cool link 2</MenuItem>
