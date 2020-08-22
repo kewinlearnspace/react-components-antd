814c2cbe936caefbbcc9f7b47d38d6ceafe10340
kewin <404502753@qq.com>
2020-08-22 10:38:49 +0800

    SubMenu测试用例

--- src/componsnts/Menu/menu.test.tsx
+++ src/componsnts/Menu/menu.test.tsx
@@ -1,25 +1,54 @@
 import React from 'react';
-import { render, RenderResult, fireEvent, cleanup } from '@testing-library/react';
+import { render, RenderResult, fireEvent, cleanup, wait } from '@testing-library/react';
 import Menu, { IMenuProps } from './menu';
 import MenuItem, { IMenuItemProps } from './menuItem';
-
+import SubMenu from './subMenu'
 const testProps: IMenuProps = {
-  defaultIndex: 0,
+  defaultIndex: '0',
   onSelect: jest.fn(),
   className: 'test'
 }
 
 const testVerProps: IMenuProps = {
-  defaultIndex: 0,
-  mode: 'vertical'
+  defaultIndex: '0',
+  mode: 'vertical',
+  defaultOpenSubMenus: ['4']
 }
 
 const generateMenu = (props: IMenuProps) => {
-  return (<Menu {...props}>
-    <MenuItem>active</MenuItem>
-    <MenuItem disabled>disabled</MenuItem>
-    <MenuItem> index </MenuItem>
-  </Menu>)
+  return (
+    <Menu {...props}>
+      <MenuItem>active</MenuItem>
+      <MenuItem disabled>disabled</MenuItem>
+      <MenuItem> index </MenuItem>
+      <SubMenu title="dropdown">
+        <MenuItem>
+          drop1
+        </MenuItem>
+      </SubMenu>
+      <SubMenu title="opened">
+        <MenuItem>
+          opened1
+        </MenuItem>
+      </SubMenu>
+    </Menu>
+  )
+}
+
+// 创建css样式文件
+const createStyleFile = () => {
+  const cssFile: string = `
+  .kewin-submenu {
+    display: none;
+  }
+  .kewin-submenu.menu-opened {
+    display:block;
+  }`
+
+  const style = document.createElement('style')
+  style.type = 'text/css'
+  style.innerHTML = cssFile
+  return style
 }
 
 /**
@@ -30,7 +59,8 @@ let wrapper: RenderResult, wrapper2: RenderResult, menuElement: HTMLElement, act
 describe('test Menu and MenuItem component in default(horizontal) mode', () => {
   beforeEach(() => {
     wrapper = render(generateMenu(testProps))
-    // wrapper.container.append(createStyleFile())
+    // 将节点插入
+    wrapper.container.append(createStyleFile())
     menuElement = wrapper.getByTestId('test-menu')
     activeElement = wrapper.getByText('active')
     disabledElement = wrapper.getByText('disabled')
@@ -39,8 +69,8 @@ describe('test Menu and MenuItem component in default(horizontal) mode', () => {
     expect(menuElement).toBeInTheDocument()
     // 测试class名称
     expect(menuElement).toHaveClass('kewin-menu test')
-    // 测试有3个li
-    expect(menuElement.querySelectorAll(':scope > li').length).toEqual(3)
+    // li的个数
+    expect(menuElement.querySelectorAll(':scope > li').length).toEqual(5)
     // 
     expect(activeElement).toHaveClass('menu-item is-active')
     expect(disabledElement).toHaveClass('menu-item is-disabled')
@@ -51,10 +81,10 @@ describe('test Menu and MenuItem component in default(horizontal) mode', () => {
     fireEvent.click(thirdItem)
     expect(thirdItem).toHaveClass('is-active')
     expect(activeElement).not.toHaveClass('is-active')
-    expect(testProps.onSelect).toHaveBeenCalledWith(2)
+    expect(testProps.onSelect).toHaveBeenCalledWith('2')
     fireEvent.click(disabledElement)
     expect(disabledElement).not.toHaveClass('is-active')
-    expect(testProps.onSelect).not.toHaveBeenCalledWith(1)
+    expect(testProps.onSelect).not.toHaveBeenCalledWith('1')
   })
   it('should render vertical mode when mode is set to vertical', () => {
     // cleanup将beforeEach执行的代码清除
@@ -62,5 +92,46 @@ describe('test Menu and MenuItem component in default(horizontal) mode', () => {
     const wrapper = render(generateMenu(testVerProps))
     const menuElement = wrapper.getByTestId('test-menu')
     expect(menuElement).toHaveClass('menu-vertical')
+  })
+  // SubMenu悬浮测试
+  it('should show dropdown items when hover on subMenu', async () => {
+    // queryByText返回htmlElment或者是null
+    expect(wrapper.queryByText('drop1')).not.toBeVisible()
+    // getByText获取到(  )文字
+    const dropdownElement = wrapper.getByText('dropdown')
+    // 进入到某个元素中
+    fireEvent.mouseEnter(dropdownElement)
+    // text为drop1的节点是否显示
+    // 由于SubMenu使用了定时器,定时器属于异步的一种,所以这里使用async await来解决
+    await wait(() => {
+      expect(wrapper.queryByText('drop1')).toBeVisible()
+    })
+    fireEvent.click(wrapper.getByText('drop1'))
+    expect(testProps.onSelect).toHaveBeenLastCalledWith('3-0')
+    // 鼠标移开后消失
+    fireEvent.mouseLeave(dropdownElement)
+    await wait(() => {
+      expect(wrapper.queryByText('drop1')).not.toBeVisible()
+    })
+  })
+  // SubMenu点击测试
+})
+describe('test Menu and MenuItem component in vertical mode', () => {
+  beforeEach(() => {
+    wrapper2 = render(generateMenu(testVerProps))
+    wrapper2.container.append(createStyleFile())
+  })
+  it('should render vertical mode when mode is set to vertical', () => {
+    const menuElement = wrapper2.getByTestId('test-menu')
+    expect(menuElement).toHaveClass('menu-vertical')
+  })
+  it('should show dropdown items when click on subMenu for vertical mode', () => {
+    const dropDownItem = wrapper2.queryByText('drop1')
+    expect(dropDownItem).not.toBeVisible()
+    fireEvent.click(wrapper2.getByText('dropdown'))
+    expect(dropDownItem).toBeVisible()
+  })
+  it('should show subMenu dropdown when defaultOpenSubMenus contains SubMenu index', () => {
+    expect(wrapper2.queryByText('opened1')).toBeVisible()
   })
 })
