ea08565651422cb398856f289cc56ac0e8321331
kewin <404502753@qq.com>
2020-08-28 23:04:40 +0800

    autoComplete自定义模板

--- src/componsnts/AutoComplete/autoComplete.stories.tsx
+++ src/componsnts/AutoComplete/autoComplete.stories.tsx
@@ -1,18 +1,49 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
-import { AutoComplete } from './autoComplete'
+import { AutoComplete, DataSourceType } from './autoComplete'
+
+interface LakerPlayerProps {
+  value: string,
+  number: number
+}
 
 const SimpleComplete = () => {
   const lakers = ['bradley', 'pope', 'caruso', 'cook', 'cousins',
     'james', 'AD', 'green', 'howard', 'kuzma', 'McGee', 'rando']
   const handleFetch = (query: string) => {
-    return lakers.filter(name => name.includes(query))
+    return lakers.filter(name => name.includes(query)).map(item => ({ value: item }))
   }
+
+  // const lakersWithNumber = [
+  //   { value: 'bradley', number: 11 },
+  //   { value: 'pope', number: 1 },
+  //   { value: 'caruso', number: 4 },
+  //   { value: 'cook', number: 2 },
+  //   { value: 'cousins', number: 15 },
+  //   { value: 'james', number: 23 },
+  //   { value: 'AD', number: 3 },
+  //   { value: 'green', number: 14 },
+  //   { value: 'howard', number: 39 },
+  //   { value: 'kuzma', number: 0 },
+  // ]
+  // const handleFetch = (query: string) => {
+  //   return lakersWithNumber.filter(name => name.value.includes(query))
+  // }
+  // const renderOption = (item: DataSourceType<LakerPlayerProps>) => {
+  //   return (
+  //     <>
+  //       <h2>name:{item.value}</h2>
+  //       <h2>number:{item.number}</h2>
+  //     </>
+  //   )
+
+  // }
   return (
     <AutoComplete
       fetchSuggestions={handleFetch}
       onSelect={action('selected')}
+    // renderOption={renderOption}
     />
   )
 }
--- src/componsnts/AutoComplete/autoComplete.tsx
+++ src/componsnts/AutoComplete/autoComplete.tsx
@@ -1,19 +1,27 @@
-import React, { FC, useState, ChangeEvent } from 'react';
+import React, { FC, useState, ChangeEvent, ReactElement } from 'react';
 import Input, { InputProps } from '../Input/input';
+
+interface DataSourceObject {
+  value: string
+}
+
+export type DataSourceType<T = {}> = T & DataSourceObject
 
 // Omit忽略掉相同的属性
 export interface IAutoComplete extends Omit<InputProps, 'onSelect'> {
-  fetchSuggestions: (str: string) => string[],
-  onSelect?: (item: string) => void
+  fetchSuggestions: (str: string) => DataSourceType[],
+  onSelect?: (item: DataSourceType) => void,
+  // 自定义模板
+  renderOption?: (item: DataSourceType) => ReactElement
 }
 
 
 export const AutoComplete: FC<IAutoComplete> = (props) => {
-  const { fetchSuggestions, value, onSelect, ...restProps } = props
+  const { fetchSuggestions, value, onSelect, renderOption, ...restProps } = props
   // 存放输入框的值
   const [inputValue, setInputValue] = useState(value)
   // 存放下拉列表的值
-  const [suggestions, setSuggestions] = useState<string[]>([])
+  const [suggestions, setSuggestions] = useState<DataSourceType[]>([])
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value.trim()
     setInputValue(value)
@@ -25,18 +33,25 @@ export const AutoComplete: FC<IAutoComplete> = (props) => {
     }
   }
 
-  const handleSelect = (item: string) => {
-    setInputValue(item)
+  const handleSelect = (item: DataSourceType) => {
+    setInputValue(item.value)
     setSuggestions([])
     if (onSelect) {
       onSelect(item)
     }
   }
+  // 自定义模板
+  const renderTemplate = (item: DataSourceType) => {
+    return renderOption ? renderOption(item) : item.value
+  }
   // 显示下拉列表
   const generateDropdown = () => {
     return <ul>
       {
-        suggestions.map((item, index) => <li key={index} onClick={() => handleSelect(item)}> {item} </li>)
+        suggestions.map((item, index) => <li key={index} onClick={() => handleSelect(item)}>
+          {renderTemplate(item)}
+        </li>
+        )
       }
     </ul>
   }
